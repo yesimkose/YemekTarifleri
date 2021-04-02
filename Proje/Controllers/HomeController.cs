69fc0717cbf66d0239f95490471d4265314549b7
koseyesim06 <koseyesim06@gmail.com>
2021-04-02 12:48:39 +0300

    Proje dosyası ekle.

---
+++ Proje/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using Proje.Data;
+using Proje.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly IStringLocalizer<HomeController> _localizer;
+
+        public HomeController(ApplicationDbContext context, IStringLocalizer<HomeController> localizer)
+        {
+            _context = context;
+            _localizer = localizer;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var db = _context.Yemek
+                .Include(f => f.Kategori);//FK lar eklenmeli.
+            var model = await db.Take(3).ToListAsync();
+
+            return View(model);
+
+        }
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
